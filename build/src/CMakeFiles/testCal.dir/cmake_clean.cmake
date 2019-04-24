8349fe79006278155b70d140599c6245974e2ac3
lingqiujin <lingqiujin@gmail.com>
2019-04-24 15:02:12 -0400

    first commit

---
+++ build/src/CMakeFiles/testCal.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testCal.dir/testCal.cpp.o"
+  "../../bin/testCal.pdb"
+  "../../bin/testCal"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/testCal.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
